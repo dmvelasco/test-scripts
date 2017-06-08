696d558760375c4454abce58cda35fb58ce9a52c
Dianne Velasco <dmvelasco@ucdavis.edu>
2017-06-08 09:40:47 -0700

    updates to pipeline scripts

--- GATK_2_map.sh
+++ GATK_2_map.sh
@@ -1,39 +1,53 @@
 #!/bin/bash -l
-#SBATCH -D /home/dmvelasc/Projects/Prunus/Data/BAM
+#SBATCH -D /group/jrigrp3/Velasco/Prunus/BAM/
 #SBATCH -o /home/dmvelasc/Projects/Prunus/slurm-log/%A_%a-GATK2map-stdout.txt
 #SBATCH -e /home/dmvelasc/Projects/Prunus/slurm-log/%A_%a-GATK2map-stderr.txt
 #SBATCH -J map
-#SBATCH -p bigmemm
-#SBATCH -a 1-2
-#SBATCH -t 16-00:00:00
+#SBATCH -p med
+#SBATCH -a 1-67%10
+#SBATCH -t 20-00:00:00
 #SBATCH -n 1
-#SBATCH -c 4
+#SBATCH -c 10
 #SBATCH --mem=24000
+#SBATCH --mail-user=dmvelasco@ucdavis.edu
+#SBATCH --mail-type=ALL
 set -e
 set -u
 
-# %A is array job ID
-# %a is array job index
-
-##### PREREQUISITE STEPS: declare variables and arrays (check), load zlib (check), make mapping directory (check), copy reference (check)
+##### PREREQUISITE STEPS:
+# declare variables and arrays
+# load zlib
+# make mapping directory
+# copy reference
 
 # Declare number variables
 x=$SLURM_ARRAY_TASK_ID
 i=$(( x-1 ))
 
-# Declare prefix array
-#declare -a abbr=(PR01 PC01 PS02 PK01 PU01 PT01 PV02 PD01 PP15 PF01 PD02 PB01 PD03 PD04 PD05 PD06 PD07 PD08 PD09 PD10)
-# Public sequences
-#declare -a abbr=(PD11 PD12 PD13 PD14 PG01 PP01 PP02 PP03 PP04 PP05 PP06 PP07 PP08 PP09 PP10 PP12 PP14 PS01 PV01 PV03 PV04 PV05 PV06 PG02 PG04 PG05 PS04 PM01 PM02 PM03 PM04 PM05 PM06 PP37 PP39 PP40 PD16 PD17 PD18 PD21 PG03 PS03 PP38 PD20 PD19 PP11 PP13)
-declare -a abbr=(PP14_A PP14_B)
-acc="${abbr["$i"]}"
+##### CREATE SAMPLE PREFIX #####
+# path to sample list
+sample="/home/dmvelasc/Projects/Prunus/Script/sample.txt"
 
-# Declare directories
+# mapfile to extract sample ID and read name information, each line is array item
+mapfile -s "$i" -n 1 -t id < "${sample}"
+# -s number of rows to skip
+# -n number of rows to read
+# -t (cannot remember, but does not correspond to last item; remove leading/trailing whitespace?)
+# id the array name
+
+# create an array from each two column line
+arr=(`echo "${id[0]}"`)
+
+# declare variables, created from array
+acc="${arr[0]}"
+
+##### Declare directories
 dir1="/home/dmvelasc/bin"				# program directory
 dir2="/home/dmvelasc/Data/references/bwa-peach-scf"	# reference directory
-dir3="/home/dmvelasc/Projects/Prunus/Data/fastq"	# sequence directory prefix
-dir4="/home/dmvelasc/Projects/Prunus/Data/BAM"		# output directory
+dir3="/group/jrigrp3/Velasco/Prunus/fastq"		# sequence directory prefix
+dir4="/group/jrigrp3/Velasco/Prunus/BAM"		# output directory
 dir5="/home/dmvelasc/Data/references/persica-SCF"	# FASTA reference directory
+scratch="/scratch/dmvelasc"				# scratch directory for temporary storage
 
 # Load zlib 1.2.8
 module load zlib
@@ -42,32 +56,29 @@ echo "create temporary directory to process sample";
 date
 
 # Make directory to copy reference
-mkdir -p "$acc"_temp
+mkdir -p "$scratch"/"$acc"_temp
 
 echo "copy reference files to temporary directory";
 date
 
 # Index reference to temp directories
-cp "$dir2"/Prunus_persica_v1.0_scaffolds.fa.* "$acc"_temp/
-cp "$dir5"/Prunus_persica_v1.0_scaffolds.fa "$acc"_temp/
-
+cp "$dir2"/Prunus_persica_v1.0_scaffolds.fa.* "$scratch"/"$acc"_temp/
+cp "$dir5"/Prunus_persica_v1.0_scaffolds.fa "$scratch"/"$acc"_temp/
 
 echo "Map ${acc} to reference with BWA MEM";
 date
 
 # Map sample to reference
 # Use below with fastq that does not need the sequence ID trimmed, i.e. non-SRR samples
-srun "$dir1"/bwa mem -M -t 4 -k 10 "$acc"_temp/Prunus_persica_v1.0_scaffolds.fa "$dir3"/"$acc"_1_filt.fq.gz "$dir3"/"$acc"_2_filt.fq.gz | "$dir1"/samtools view -T "$acc"_temp/Prunus_persica_v1.0_scaffolds.fa - -o "$dir4"/"$acc".bam
-# Use below with fastq that DOES need the sequence ID trimmed, i.e. SRR samples
-#srun "$dir1"/bwa mem -M -t 4 -k 10 "$acc"_temp/Prunus_persica_v1.0_scaffolds.fa "$dir3"/"$acc"_1_filt2.fq.gz "$dir3"/"$acc"_2_filt2.fq.gz | "$dir1"/samtools view -T "$acc"_temp/Prunus_persica_v1.0_scaffolds.fa - -o "$dir4"/"$acc".bam
-
+srun "$dir1"/bwa mem -M -t 10 -k 10 "$scratch"/"$acc"_temp/Prunus_persica_v1.0_scaffolds.fa "$dir3"/"$acc"_1_filt.fq.gz "$dir3"/"$acc"_2_filt.fq.gz | "$dir1"/samtools view -T "$scratch"/"$acc"_temp/Prunus_persica_v1.0_scaffolds.fa - -o "$scratch"/"$acc".bam
+mv "$scratch"/"$acc".bam "$dir4"/
 # -t	threads
 # -M	Mark shorter split hits as secondary (for Picard compatibility)
 # -k	Minimum seed length [default 19]
 # -r	Trigger re-seeding for a MEM longer than minSeedLen*FLOAT [1.5]; previously used 2.85 but may lead to lower accuracy
 
-echo "remove ${acc}_temp directory";
+echo "remove ${scratch}/${acc}_temp directory";
 date
 
 # remove temporary reference directory recursively
-rm -r "$acc"_temp/
+rm -r "$scratch"/"$acc"_temp/
--- fastqmcf.sh
+++ fastqmcf.sh
@@ -29,16 +29,9 @@ i=$(( x-1 ))
 
 # Declare variables
 # path to FASTA adapter file
-adapter="/home/dmvelasc/Projects/Prunus/Script/adapter.fa
+adapter="/home/dmvelasc/Projects/Prunus/Script/adapter.fa"
 # path to sample list
 sample="/home/dmvelasc/Projects/Prunus/Script/sample.txt"
-
-# Declare prefix array <- if mapfile below works for array jobs then remove this - testing appears that it will work
-#declare -a main=(DPRU0194 DPRU0579 DPRU0582 DPRU1467.9 DPRU1871.1 DPRU2327.16 DPRU2493.7 DPRU2578.2 Lovell fenzliana TNP arabica DPRU1791.3 DPRU2374.12 DPRU1456.4 DPRU2301 DPRU1462.2 DPRU1207.2 DPRU2331.9 DPRU0210 SRR068360 SRR068361 SRR502982 SRR502983 SRR502984 SRR502985 SRR502986 SRR502987 SRR502990 SRR502992 SRR502994 SRR502995 SRR502997 SRR502998 SRR503000 SRR765679 SRR765838 SRR765850 SRR765861 SRR3237762 SRR3138171 SRR3141016 SRR3141018 SRR3138115 SRR3138121 SRR3138123 SRR3138169 SRR3237746 SRR3141019 SRR3136174 SRR3136179 SRR3136181 SRR3136183 SRR765861 SRR3138129 SRR3138145 SRR3138147 SRR3141049 SRR3141073 SRR3141113 SRR3141248 SRR3138117 SRR3138168 SRR3138132 SRR3141238 SRR3141181)
-#declare -a abbr=(PR01 PC01 PS02 PK01 PU01 PT01 PV02 PD01 PP15 PF01 PD02 PB01 PD03 PD04 PD05 PD06 PD07 PD08 PD09 PD10 PP14 PP12 PV01 PP08 PS01 PP01 PP06 PP05 PP04 PP03 PP02 PP10 PP09 PG01 PP07 PD14 PD13 PD12 PD11 PV03 PV04 PV05 PV06 PG02 PG04 PG05 PS04 PM01 PM02 PM03 PM04 PM05 PM06 PD11 PP37 PP39 PP40 PD16 PD17 PD18 PD21 PG03 PS03 PP38 PD20 PD19)
-
-#reads="${main["$i"]}"
-#acc="${abbr["$i"]}"
 
 # mapfile to extract sample ID and read name information, each line is array item
 mapfile -s "$i" -n 1 -t id < "${sample}"
@@ -51,8 +44,8 @@ mapfile -s "$i" -n 1 -t id < "${sample}"
 arr=(`echo "${id[0]}"`)
 
 # declare variables, created from array
-reads="${arr[0]}"
-acc="${arr[1]}"
+reads="${arr[1]}"
+acc="${arr[0]}"
 
 # run fastq-mcf to trim reads
 fastq-mcf -t16 "$adapter" "$reads"_1.fastq.gz -o "$acc"_1_filt.fq.gz "$reads"_2.fastq.gz  -o "$acc"_2_filt.fq.gz
