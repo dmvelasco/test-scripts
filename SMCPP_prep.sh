0e6b54394cbb30efc10db35a09dd850d0cd18bbf
Dianne Velasco <dmvelasco@ucdavis.edu>
2017-08-10 21:21:34 -0700

    SMC++ script nearly running, occasional memory issues

--- SMCPP_prep.sh
+++ SMCPP_prep.sh
@@ -6,20 +6,22 @@
 #SBATCH -p bigmemm
 #SBATCH -t 10-00:00:00
 #SBATCH -n 1
-#SBATCH -c 4
+#SBATCH -c 8
 #SBATCH --mail-user=dmvelasco@ucdavis.edu
 #SBATCH --mail-type=ALL
-#SBATCH --mem=8G
+#SBATCH --mem=32G
 set -e
 set -u
 
 ########## WRITTEN BY D. VELASCO ###########
+###### borrowed heavily from M Stetter #####
 
 ####################
 ### Load modules ###
 ####################
 # Anaconda 3, automatically enables SMC++ (and Mafft)
 module load conda3
+module load vcftools
 module load tabix
 
 #############################
@@ -33,16 +35,19 @@ module load tabix
 #declare -a id=(PC01 PD03 PD04 PD05 PD06 PD07 PD08 PD09 PR01 PS02)
 #sample="${id["$i"]}"
 
-# genome reference file location
+# path for genome reference file
 genome="/home/dmvelasc/Data/references/persica-SCF/Prunus_persica_v1.0_scaffolds.fa"
-# file directory for $sample vcf to convert to fasta
+# path for joint VCF file - test joint VCF file
 vcf="/home/dmvelasc/Projects/Prunus/Analysis/VCF_GATK/test_jointcalls.vcf"
-smc_in="/home/dmvelasc/Projects/Prunus/Data/smcpp_input/test_jointcalls.smc.gz"
+# path for filtered joint VCF file - dulcis test VCF file
+vcf_filt="/home/dmvelasc/Projects/Prunus/Analysis/VCF_GATK/test_dulcis.recode.vcf"
+# path for SMC++ prepped file
+smc_in="/home/dmvelasc/Projects/Prunus/Data/smcpp_input/"
 
 
 # Declare directories and file prefix
-dir1="/home/dmvelasc/Software/bedtools2/bin"		# bedtools location
-dir2="/home/dmvelasc/Projects/Prunus/Data/BAM"		# BAM directory
+#dir1="/home/dmvelasc/Software/bedtools2/bin"		# bedtools location
+#dir2="/home/dmvelasc/Projects/Prunus/Data/BAM"		# BAM directory
 dir3="/home/dmvelasc/Data/references/persica-SCF"	# gff3 location
 dir4="/scratch/dmvelasc"
 
@@ -53,40 +58,38 @@ echo -e "begin SMC++ preparation\n get individuals"
 date
 
 # select individuals
-module load vcftools
-vcftools --gzvcf "$vcf".gz --indv PD03 --indv PD04 --indv PD05 --indv PD06 --indv PD07 --indv PD08 --indv PD09 --min-alleles 2 --max-alleles 2 --recode --out dulcis
+vcftools --vcf "$vcf" --indv PD03 --indv PD04 --indv PD05 --indv PD06 --indv PD07 --indv PD08 --indv PD09 --min-alleles 2 --max-alleles 2 --recode --out test_dulcis
+mv /home/dmvelasc/Projects/Prunus/Analysis/smcpp/test_dulcis.recode.vcf /home/dmvelasc/Projects/Prunus/Analysis/VCF_GATK/
 
-echo -e "begin SMC++ preparation\n convert vcf file to SMC++ format file"
+echo -e "convert vcf file to SMC++ format file"
 date
 
-bgzip "$vcf" > "$vcf".gz
-tabix -fp vcf "$vcf".gz
-smc++ vcf2smc --missing-cutoff 1000 --length 80000000 "$vcf".gz "$smc_in" scaffold_1 Pop1:PD03,PD04,PD05,PD06,PD07,PD08,PD09
+bgzip -f "$vcf_filt" > "$vcf_filt".gz
+tabix -fp vcf "$vcf_filt".gz
+#smc++ vcf2smc --missing-cutoff 1000 --length 80000000 "$vcf_filt".gz "$smc_in"/test_dulcis_1.smc.gz scaffold_1 Pop1:PD03,PD04,PD05,PD06,PD07,PD08,PD09
+# above is modified vcf2smc line, however, put into for loop below to iterate over first eight scaffolds of Prunus
+
 # Markus' original SMC++ line
 #smc++ vcf2smc --missing-cutoff 1000 --length 80000000 chr10_282_4ind.vcf.gz smc_input/chr10_282_4ind.smc.gz 10 Pop1:282set_B73,282set_W22,282set_Ki11,282set_T8
-#after smc file 10 Pop1:232set_B73,282set_W22,282set_Ki11,282set_T8
+#after smc_input/file 10 Pop1:232set_B73,282set_W22,282set_Ki11,282set_T8
 # 10	chromosome number
 # Pop1:282set_B73,282set_W22,282set_Ki11,282set_T8
 # population and members of the population
+# --missing-cutoff is the length of runs of homozygosity to ignore, assumes missing data
+# --length is length of the chromosome, default uses header length <- does vcf tools or other manipulations remove header?
 
-
-#run array with each chromosome, not sure needed for prunus b/c small genome
-#SBATCH -a 1-10
-
-echo -e "begin SMC++ preparation\n for each chromosome"
+echo -e "Run for loop by chromosome as per smcpp instructions"
 date
 
-scaf=$SLURM_ARRAY_TASK_ID
-module load tabix
-
-bgzip -c > "$vcf".gz
-tabix -fp vcf "$vcf".gz
+for i in {1..8}; do
+  smc++ vcf2smc --missing-cutoff 5000 "$vcf_filt".gz "$smc_in"/test_dulcis_"$i".smc.gz scaffold_"$i" Pop1:PD03,PD04,PD05,PD06,PD07,PD08,PD09
+done
 
 
 echo -e "begin SMC++ analysis"
 date
 # SMC++ analysis
-smc++ estimate -o smc_analysis/ 3e-8 smc_input/*.smc.gz
+smc++ estimate -o smc_analysis/ 3e-8 "$smc_in"/*.smc.gz
 #--polarization-error 0.5
 # --polarization-error: if the identity of the ancestral allele is not known,
 # these options can be used to specify a prior over it. With polarization error p,
@@ -96,77 +99,15 @@ smc++ estimate -o smc_analysis/ 3e-8 smc_input/*.smc.gz
 # --unfold is an alias for --polarization-error 0. If the ancestral allele is known
 # (from an outgroup, say) then this option will use the unfolded SFS for computing
 # probabilities. Incorrect usage of this feature may lead to erroneous results.
-#3e-8 is per generation mutation rate, will probably need to run with three different values (Xie et al.)
+#3e-8 is per generation mutation rate, will probably need to run with three different values based on Xie et al.
 
 echo -e "plot SMC++ results"
 date
-smc++ plot history_all_282.pdf analysis/model.final.json
+smc++ plot history_dulcis.pdf smc_analysis/model.final.json
 #
 
 #-g	sets generation time in years to scale x-axis, otherwise in coalescent units
 #--logy	plots the y-axis on a log scale
 #-c	produces CSV-formatted table containing the data used to generate the plot
 
-###### From M Stetter
 
-###### Convert VCF
-#!/bin/bash -l
-#SBATCH -D /home/mstetter/smcpp_test/
-#SBATCH -o /home/mstetter/smcpp_test/stdout-%j.txt
-#SBATCH -J extract_282
-#SBATCH -t 00:05:00
-set -e
-set -u
-
-module load tabix
-
-source /home/mstetter/tools/smcpp/bin/activate
-bgzip chr10_282_4ind.vcf
-tabix -fp vcf chr10_282_4ind.vcf.gz
-smc++ vcf2smc --missing-cutoff 1000 --length 80000000 chr10_282_4ind.vcf.gz smc_input/chr10_282_4ind.smc.gz 10 Pop1:282set_B73,282set_W22,282set_Ki11,282set_T8
-
-
-
-#!/bin/bash -l
-#SBATCH -D /home/mstetter/282_analysis/
-#SBATCH -o /home/mstetter/logs/smcpp-%j.txt
-#SBATCH -J vcf2smc
-#SBATCH -t 2-00:00
-#SBATCH --array=1-10
-set -e
-set -u
-
-
-hset=$SLURM_ARRAY_TASK_ID
-module load tabix
-
-gunzip -c /group/jrigrp/Share/genotypes/282_7X/c${hset}_282_corrected_onHmp321.vcf.gz | bgzip -c > c${hset}_282_corrected_onHmp321.vcf.gz
-tabix -fp vcf c${hset}_282_corrected_onHmp321.vcf.gz
-
-
-###### Get individuals
-#!/bin/bash -l
-#SBATCH -D /home/mstetter/smcpp_test/
-#SBATCH -o /home/mstetter/smcpp_test/stdout-%j.txt
-#SBATCH -J extract_282
-#SBATCH -t 00:05:00
-set -e
-set -u
-
-module load vcftools
-vcftools --gzvcf /group/jrigrp/Share/genotypes/282_7X/c10_282_corrected_onHmp321.vcf.gz --indv 282set_B73 --indv 282set_W22 --indv 282set_Ki11 --indv 282set_T8 --min-alleles 2 --max-alleles 2 --recode --out chr10_282_4ind
-
-##### SMC analysis
-#!/bin/bash -l
-#SBATCH -D /home/mstetter/smcpp_test/
-#SBATCH -o /home/mstetter/smcpp_test/stdout-%j.txt
-#SBATCH -J smcpp_analysis
-#SBATCH -t 04:00:00
-#SBATCH -p bigmemm
-set -e
-set -u
-
-source /home/mstetter/tools/smcpp/bin/activate
-
-smc++ estimate -o smc_analysis/ --polarization-error 0.5 3e-8 smc_input/*.smc.gz
-smc++ plot history_all_282.pdf analysis/model.final.json
