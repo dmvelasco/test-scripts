cf4fd25e591e9116b31cbc33b8a18a5fbf29d66d
Dianne Velasco <dmvelasco@ucdavis.edu>
2017-08-23 11:58:22 -0700

    updates to SMCPP.sh for persica (pop PP) and to streamline functioning so fewer modifications required to run different sample sets

--- SMCPP.sh
+++ SMCPP.sh
@@ -34,10 +34,9 @@ genome="/home/dmvelasc/Data/references/persica-SCF/Prunus_persica_v1.0_scaffolds
 # Joint VCF file - test joint VCF file
 vcf="/home/dmvelasc/Projects/Prunus/Analysis/VCF_GATK/all_jointcalls.vcf"
 # filtered joint VCF file - dulcis test VCF file
-vcf_filt="/home/dmvelasc/Projects/Prunus/Analysis/VCF_GATK/all_persica.recode.vcf"
-# SMC++ prepped file
+vcf_filt="/home/dmvelasc/Projects/Prunus/Analysis/VCF_GATK"
+# SMC++ prepped file directory
 smc_in="/home/dmvelasc/Projects/Prunus/Data/smcpp_input/"
-final="mu1_38x-8_allpersica" #final directory name
 
 ####### PARAMETERS #######
 mu="1.38e-8"	# population mutation rate
@@ -51,44 +50,50 @@ echo -e "begin SMC++ preparation\n get individuals"
 date
 
 # select individuals
-# PD; dulcis
-#vcftools --vcf "$vcf" --indv PD02 --indv PD03 --indv PD04 --indv PD05 --indv PD06 --indv PD07 --indv PD08 --indv PD09 --indv PD10 --indv PD11 --indv PD12 --indv PD13 --indv PD14 --indv PD16 --indv PD17 --indv PD18 --indv PD20 --indv PD21 --min-alleles 2 --max-alleles 2 --recode --out all_"$pop"
-# PP; persica
-vcftools --vcf "$vcf" --indv PP02 --indv PP03 --indv PP04 --indv PP05 --indv PP06 --indv PP08 --indv PP10 --indv PP11 --indv PP13 --indv PP14 --indv PP15 --indv PP37 --indv PP38 --indv PP39 --indv PP40 --min-alleles 2 --max-alleles 2 --recode --out all_"$pop"
-# PM; mira
-#vcftools --vcf "$vcf" --indv PM01 --indv PM02 --indv PM03 --indv PM04 --indv PM05 --indv PM06 --min-alleles 2 --max-alleles 2 --recode --out all_"$pop"
-# PV; davidiana
-#vcftools --vcf "$vcf" --indv PV01 --indv PV02 --indv PV03 --indv PV04 --indv PV05 --indv PV06 --min-alleles 2 --max-alleles 2 --recode --out all_"$pop"
-# PS; kansuensis
-#vcftools --vcf "$vcf" --indv PS01 --indv PS02 --indv PS03 --indv PS04 --min-alleles 2 --max-alleles 2 --recode --out all_"$pop"
-# PG; ferganensis
-#vcftools --vcf "$vcf" --indv PG02 --indv PG03 --indv PG04 --indv PG05 --min-alleles 2 --max-alleles 2 --recode --out all_"$pop"
+# PD; dulcis; subset=all; 18 individuals; 12 CPU
+#sub="all" #subset name
+#vcftools --vcf "$vcf" --indv PD02 --indv PD03 --indv PD04 --indv PD05 --indv PD06 --indv PD07 --indv PD08 --indv PD09 --indv PD10 --indv PD11 --indv PD12 --indv PD13 --indv PD14 --indv PD16 --indv PD17 --indv PD18 --indv PD20 --indv PD21 --min-alleles 2 --max-alleles 2 --recode --out "$sub"_"$pop"
+# PP; persica; subset=all; 14 individuals; 12 CPU
+sub="all" #subset name
+vcftools --vcf "$vcf" --indv PP02 --indv PP03 --indv PP04 --indv PP05 --indv PP06 --indv PP08 --indv PP11 --indv PP13 --indv PP14 --indv PP15 --indv PP37 --indv PP38 --indv PP39 --indv PP40 --min-alleles 2 --max-alleles 2 --recode --out "$sub"_"$pop"
+# PM; mira; subset=all; 6 individuals; 4 CPU?
+#sub="all" #subset name
+#vcftools --vcf "$vcf" --indv PM01 --indv PM02 --indv PM03 --indv PM04 --indv PM05 --indv PM06 --min-alleles 2 --max-alleles 2 --recode --out "$sub"_"$pop"
+# PV; davidiana; subset=all; 6 individuals; 4 CPU?
+#sub="all" #subset name
+#vcftools --vcf "$vcf" --indv PV01 --indv PV02 --indv PV03 --indv PV04 --indv PV05 --indv PV06 --min-alleles 2 --max-alleles 2 --recode --out "$sub"_"$pop"
+# PS; kansuensis; subset=all; 4 individuals; 4 CPU
+#sub="all" #subset name
+#vcftools --vcf "$vcf" --indv PS01 --indv PS02 --indv PS03 --indv PS04 --min-alleles 2 --max-alleles 2 --recode --out "$sub"_"$pop"
+# PG; ferganensis; subset=all; 4 individuals; 4 CPU
+#sub="all" #subset name
+#vcftools --vcf "$vcf" --indv PG02 --indv PG03 --indv PG04 --indv PG05 --min-alleles 2 --max-alleles 2 --recode --out "$sub"_"$pop"
 
-mv /home/dmvelasc/Projects/Prunus/Analysis/smcpp/all_"$pop".recode.vcf /home/dmvelasc/Projects/Prunus/Analysis/VCF_GATK/
+mv /home/dmvelasc/Projects/Prunus/Analysis/smcpp/all_"$pop".recode.vcf "$vcf_filt"/
 
 echo -e "convert vcf file to SMC++ format file"
 date
 
-bgzip -f "$vcf_filt" > "$vcf_filt".gz
-tabix -fp vcf "$vcf_filt".gz
+bgzip -f "$vcf_filt"/all_"$pop".recode.vcf > "$vcf_filt"/all_"$pop".recode.vcf.gz
+tabix -fp vcf "$vcf_filt"/all_"$pop".recode.vcf.gz
 
 echo -e "Run for loop by chromosome as per smcpp instructions"
 date
 
 for i in {1..8}; do
-#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt".gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PD02,PD03,PD04,PD05,PD06,PD07,PD08,PD09,PD10,PD11,PD12,PD13,PD14,PD16,PD17,PD18,PD20,PD21
-  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt".gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PP02,PP03,PP04,PP05,PP06,PP08,PP10,PP11,PP13,PP14,PP15,PP37,PP38,PP39,PP40
-#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt".gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PM01,PM02,PM03,PM04,PM05,PM06
-#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt".gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PV01,PV02,PV03,PV04,PV05,PV06
-#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt".gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PS01,PS02,PS03,PS04
-#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt".gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PG02,PG03,PG04,PG05
+#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt"/all_"$pop".recode.vcf.gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PD02,PD03,PD04,PD05,PD06,PD07,PD08,PD09,PD10,PD11,PD12,PD13,PD14,PD16,PD17,PD18,PD20,PD21
+  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt"/all_"$pop".recode.vcf.gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PP02,PP03,PP04,PP05,PP06,PP08,PP11,PP13,PP14,PP15,PP37,PP38,PP39,PP40
+#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt"/all_"$pop".recode.vcf.gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PM01,PM02,PM03,PM04,PM05,PM06
+#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt"/all_"$pop".recode.vcf.gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PV01,PV02,PV03,PV04,PV05,PV06
+#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt"/all_"$pop".recode.vcf.gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PS01,PS02,PS03,PS04
+#  smc++ vcf2smc --missing-cutoff "$cut" "$vcf_filt"/all_"$pop".recode.vcf.gz "$smc_in"/all_"$pop"_"$i".smc.gz scaffold_"$i" "$pop":PG02,PG03,PG04,PG05
 done
 
 
 echo -e "begin SMC++ analysis"
 date
 # SMC++ analysis
-smc++ estimate -o smc_analysis/ "$mu" "$smc_in"/*.smc.gz
+smc++ estimate -o smc_analysis/ "$mu" "$smc_in"/*"$pop"*.smc.gz
 
 #--polarization-error 0.5
 # --polarization-error: if the identity of the ancestral allele is not known,
@@ -109,8 +114,8 @@ smc++ plot history_"$pop"_"$mu".pdf smc_analysis/model.final.json
 #-c	produces CSV-formatted table containing the data used to generate the plot
 
 # move files output files to subdirectory
-mkdir -p smc_analysis/"$final"
-mv smc_analysis/model.final.json smc_analysis/"$final"
-mv smc_analysis/.model.iter*.json smc_analysis/"$final"
-mv smc_analysis/.debug.txt smc_analysis/"$final"
+mkdir -p smc_analysis/"$pop"_"$sub"_"$mu"
+mv smc_analysis/model.final.json smc_analysis/"$pop"_"$sub"_"$mu"
+mv smc_analysis/.model.iter*.json smc_analysis/"$pop"_"$sub"_"$mu"
+mv smc_analysis/.debug.txt smc_analysis/"$pop"_"$sub"_"$mu"
 
