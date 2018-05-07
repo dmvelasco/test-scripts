0c6163c1149ba3a71447a9da69322f5eb39845c4
Dianne Velasco <dmvelasco@ucdavis.edu>
2018-05-07 15:08:02 -0700

    in progress phasing script using SAMtools, likely no longer needed

--- phase_bam.sh
+++ phase_bam.sh
@@ -1,93 +1,118 @@
 #!/bin/bash -l
-#SBATCH -D /home/dmvelasc/Projects/Prunus/Data/BAM
-#SBATCH -o /home/dmvelasc/Projects/Prunus/slurm-log/%A_%a-map-stdout.txt
-#SBATCH -e /home/dmvelasc/Projects/Prunus/slurm-log/%A_%a-map-stderr.txt
-#SBATCH -J bwa2bam
+#SBATCH -D /group/jrigrp3/Velasco/Prunus/BAM
+#SBATCH -o /home/dmvelasc/Projects/Prunus/slurm-log/%j-phase-stdout.txt
+#SBATCH -e /home/dmvelasc/Projects/Prunus/slurm-log/%j-phase-stderr.txt
+#SBATCH -J fasta
 #SBATCH -p serial
-#SBATCH -a 1-20%5
 #SBATCH -t 8-00:00:00
 #SBATCH -n 1
-#SBATCH -c 16
-#SBATCH --mem=24000
+#SBATCH -c 8
+#SBATCH --mem=60G
 set -e
 set -u
 
-
-########## PHASE AND EXTRACT CDS AND/OR GENE FASTA FILES ##########
-# Step 1: prep prefix information to cycle through each BAM file
+########## PHASE BAMs & EXTRACT FASTA for each CDS/GENE ##########
+# In a loop?
+# Part 1:
+# 1. prep prefix information to cycle through each BAM file
 # (using the GATK produced HCrealign.bam files)
-# Step 2: use phase to output at least two new phased BAM files
-# Step 3: use view and fasta to extract each CDS/gene FASTA sequence
-# Step 4: append FASTA sequences to one file with  appropriate FASTA header for each
-#---------------------------------------
-# Step 5: create multi sequence alignment for each CDS/gene with mafft
-# Step 6: selection analysis
+# 2. use samtools phase to output at least two new phased BAM files
+# 3. use samtools view and samtools fasta to extract each CDS/gene FASTA sequence
+# 4. append FASTA sequence to one file with appropriate FASTA header for each
+# -------------------------------------------------
+# Part 2:
+# 1. create multi sequence alignment for each CDS/gene with mafft
+# 2. selection analysis
 
-##### PREREQUISITE STEPS #####
-# declare variables and arrays
-# load zlib
-# make mapping directory
-# copy reference
-
-# Declare number variables
-x=$SLURM_ARRAY_TASK_ID
-i=$(( x-1 ))
-
-# Declare prefix array
-declare -a abbr=(PR01 PC01 PS02 PK01 PU01 PT01 PV02 PD01 PP15 PF01 PD02 PB01 PD03 PD04 PD05 PD06 PD07 PD08 PD09 PD10)
-
-acc="${abbr["$i"]}"
-
-# make directory to copy reference
-#mkdir "$acc"_temp
-
-# Declare directories
-dir1="/home/dmvelasc/bin"				# program directory
-BAMdir="/group/jrigrp3/Velasco/Prunus/BAM"		# BAM directory
-dir2="/home/dmvelasc/Data/references/bwa-peach-scf"	# reference directory
-dir3="/home/dmvelasc/Projects/Prunus/Data/fastq"	# sequence directory prefix
-dir4="/home/dmvelasc/Projects/Prunus/Data/BAM"		# output directory
-dir5="/home/dmvelasc/Data/references/persica-SCF"	# FASTA reference directory
-
-# Load zlib 1.2.8
+### Load modules ###
 module load zlib
 
-# Index reference to temp directories
-cp "$dir2"/Prunus_persica_v1.0_scaffolds.fa.* "$acc"_temp/
-cp "$dir5"/Prunus_persica_v1.0_scaffolds.fa "$acc"_temp/
+### Declare directories ###
+bin="/home/dmvelasc/bin"				# program directory
+BAMdir="/group/jrigrp3/Velasco/Prunus/BAM"		# BAM directory
+ref="/home/dmvelasc/Data/references/persica-SCF"	# FASTA reference directory
+scratch="/scratch/dmvelasc"
 
-##### Map to reference with BWA MEM #####
+#### sample ID file
+# column 1: ID, column2: other ID/information
+list="/home/dmvelasc/Projects/Prunus/Script/sample.txt"
 
-srun "$dir1"/bwa mem -M -t 14 -k 10 "$acc"_temp/Prunus_persica_v1.0_scaffolds.fa "$dir3"/"$acc"_1_filt.fq.gz "$dir3"/"$acc"_2_filt.fq.gz | "$dir1"/samtools view -T "$acc"_temp/Prunus_persica_v1.0_scaffolds.fa - -o "$dir4"/"$acc".bam
-# -t	threads
-# -M	Mark shorter split hits as secondary (for Picard compatibility)
-# -k	Minimum seed length [default 19]
-# -r	Trigger re-seeding for a MEM longer than minSeedLen*FLOAT [1.5] have previously used 2.85 but may lead to lower accuracy
+# gene intervals
+intervals="/home/dmvelasc/Data/references/persica-SCF/Ppv1gene.intervals"
 
-# remove temporary reference directory recursively
-rm -r "$acc"_temp/
+loop through each bam file
+for i in {0..2}; do
+  ##### mapfile to extract sample ID, each line is array item
+  mapfile -s "$i" -n 1 -t id < "${list}"
+  # -s number of rows to skip | -n number of rows to read | -t (remove leading/trailing whitespace?)
+  # id is the array name (anything in this position is the array name)
+
+  # create an array from each two column line
+  arr=(`echo "${id[0]}"`)
+
+  # declare variables, created from array
+  acc="${arr[0]}"
+
+####### Don't need?
+#  # make directory to copy reference
+#  mkdir -p "$acc"_temp
+
+####### Don't need
+#  # Index reference to temp directories
+#  cp "$dir2"/Prunus_persica_v1.0_scaffolds.fa.* "$acc"_temp/
+#  cp "$dir5"/Prunus_persica_v1.0_scaffolds.fa "$acc"_temp/
+
+###### Don't need?
+#  # create scratch directory for temporary file placement
+#  mkdir -p /scratch/dmvelasc/"$acc"
+
+  ##### Phase with SAMtools #####
+  srun "$bin"/samtools phase -A -b "$acc"_phased -Q 20 "$BAMdir"/"$acc".HCrealign.bam
+  # out is working/default directory
+
+  echo "begin CDS FASTA script"
+  date
+
+  ##### Extract FASTA from each BAM with SAMtools #####
+
+  ##### mapfile to extract cds/gene region
+  mapfile -s "$i" -n 1 -t genes < "${intervals}"
+  # -s number of rows to skip | -n number of rows to read | -t (remove leading/trailing whitespace?)
+  # id is the array name (anything in this position is the array name)
+
+  # create an array from each line
+  cds=(`echo "${genes[0]}"`)
+#  # declare variables, created from array
+#  interval="${cds[0]}"
+
+  # do for each phased bam of the sample
+  samtools "$bin"/samtools view -u "$acc"_phased.0.bam "$cds" | "$bin"/samtools fasta -
+  samtools "$bin"/samtools view -u "$acc"_phased.1.bam "$cds" | "$bin"/samtools fasta -
+#  samtools "$bin"/samtools view -u "$acc"_phased.chimeric.bam "$cds" | "$bin"/samtools fasta
+	concatenate fastas for each region
+
+  # move phased BAMs to BAM directory?
+
+  ##### process each CDS
+
+  while read p; do
+    # basic manipulations to create final CDS FASTA
+    echo ">${acc}" > "$dir4"/"$acc"/"$p"_"$acc".fa
+    awk '{printf $0;}' "$dir4"/"$acc"/"$p"_"$acc"_cds.fa | fold -w 60 - >> "$dir4"/"$acc"/"$p"_"$acc".fa
+    rm "$scratch"/"$acc"/"$p"_"$acc"_cds.fa
+  done < "$ref"/Prunus_persica_v1.0_genes_list.gff3
+
+  # move sample file directory from scratch
+  mv /scratch/dmvelasc/"$acc"/ /home/dmvelasc/Projects/Prunus/Analysis/genetree/
+
+  echo "end CDS FASTA script"
+  date
+done
 
 
-##### SAM AND BAM CONVERSIONS, SORTING, AND CLEAN UP #####
+##### IS BELOW NEEDED?? #####
+#Simple UNIX code for splitting FASTA on '>'
+#see https://www.biostars.org/p/2226/
+#Solutions offered by users Biomonika (Noolean) and new
 
-# sort BAM file - CONFIRM MEMORY QTY
-# -@ is number of sorting and compression threads; -m is memory per thread
-srun "$dir1"/samtools sort -l 9 -@ 14 -m 1G -o "$dir4"/"$acc".sorted.bam -T "$dir4"/"$acc".sorting "$dir4"/"$acc".bam
-
-# remove unsorted BAM
-rm "$dir4"/"$acc".bam
-
-# convert back to uncompressed BAM
-"$dir1"/samtools view -bh "$dir4"/"$acc".sorted.bam -o "$dir4"/"$acc".bam
-
-# remove sorted compressed BAM, final BAM file is <prefix>.bam
-rm "$dir4"/"$acc".sorted.bam
-
-
-##### REMOVE DUPLICATE READS #####
-
-# remove duplicate reads
-"$dir1"/samtools rmdup -S "$dir4"/"$acc".bam "$dir4"/"$acc".nodup.bam
-
-# remove BAM with duplicate reads
-rm "$dir4"/"$acc".bam
+# csplit -f "$prefix"fa_ -s file.fa '/>/' {*}
