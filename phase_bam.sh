fe596356a6f84c7c221ec5baeb3839ffe9eb6ff8
Dianne Velasco <dmvelasco@ucdavis.edu>
2018-05-15 16:15:25 -0700

    Updates to SAMtools phasing script phase_bam.sh. Working through phasing command.

--- phase_bam.sh
+++ phase_bam.sh
@@ -3,11 +3,13 @@
 #SBATCH -o /home/dmvelasc/Projects/Prunus/slurm-log/%j-phase-stdout.txt
 #SBATCH -e /home/dmvelasc/Projects/Prunus/slurm-log/%j-phase-stderr.txt
 #SBATCH -J fasta
-#SBATCH -p serial
+#SBATCH -p bigmemh
 #SBATCH -t 8-00:00:00
 #SBATCH -n 1
 #SBATCH -c 8
 #SBATCH --mem=60G
+#SBATCH --mail-user=dmvelasco@ucdavis.edu
+#SBATCH --mail-type=ALL
 set -e
 set -u
 
@@ -38,11 +40,12 @@ list="/home/dmvelasc/Projects/Prunus/Script/sample.txt"
 
 # gene/cds intervals
 gene_list="Prunus_persica_v1.0_genes_list.gff3"
-gene="Prunus_persica_v1.0_gene_position_list.txt"
+gene_pos_list="Prunus_persica_v1.0_gene_position_list.txt"
 
+echo "Begin phasing script"
 
-loop through each bam file
-for i in {0..2}; do
+# loop through each bam file
+for i in {0..1}; do
   ##### mapfile to extract sample ID, each line is array item
   mapfile -s "$i" -n 1 -t id < "${list}"
   # -s number of rows to skip | -n number of rows to read | -t (remove leading/trailing whitespace?)
@@ -53,13 +56,13 @@ for i in {0..2}; do
 
   # declare variables, created from array
   acc="${arr[0]}"
+  echo -e "$acc"
 
-###### Don't need?
-#  # create scratch directory for temporary file placement
-#  mkdir -p /scratch/dmvelasc/"$acc"
+  # create scratch directory for temporary file placement
+  mkdir -p "$scratch"/"$acc"
 
   ##### Phase with SAMtools #####
-  srun "$bin"/samtools phase -A -b "$acc"_phased -Q 20 "$acc".HCrealign.bam
+  srun "$bin"/samtools phase -A -A -Q 20 -b "$acc"_phased "$acc"_HCrealign.bam
   # out is working/default directory
 
   echo "begin CDS FASTA script"
@@ -76,24 +79,29 @@ for i in {0..2}; do
     gene_id="${locus[4]}"
 
     # do for each phased bam of the sample
-    samtools "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam "$gene_interval" | "$bin"/samtools fasta - > "$gene_id"_"$acc"_0.fasta
-    samtools "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.1.bam "$gene_interval" | "$bin"/samtools fasta - > "$gene_id"_"$acc"_1.fasta
-  done < "$ref"/"$gene"
+    "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam "$gene_interval" | "$bin"/samtools fasta - > "$scratch"/"$acc"/"$gene_id"_"$acc"_0.fa
+    "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.1.bam "$gene_interval" | "$bin"/samtools fasta - > "$scratch"/"$acc"/"$gene_id"_"$acc"_1.fa
+  done < "$ref"/"$gene_pos_list"
 
   ##### process each CDS from list of gene IDs and raw GATK alternate reference FASTA maker
   # will partly depend on how samtools outputs a fasta
 
   while read q; do
-	concatenate fastas for each region
-    # create final CDS FASTA with basic file manipulations
-    # do for each phased bam of the sample
+    # create phased CDS FASTA components from BAM
+    # concatenate and create final CDS FASTA with basic file manipulations
+    touch "$scratch"/"$acc"/"$q"_"$acc"_cds_0.fasta
+    touch "$scratch"/"$acc"/"$q"_"$acc"_cds_1.fasta
     while read r; do
-      samtools "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam "$interval" | "$bin"/samtools fasta - > "$gene"_"$acc"_0.fasta
-      samtools "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.1.bam "$interval" | "$bin"/samtools fasta - > "$gene"_"$acc"_1.fasta
+      "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam "$r" | "$bin"/samtools fasta - >> "$scratch"/"$acc"/"$q"_"$acc"_cds_0.fa
+      "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.1.bam "$r" | "$bin"/samtools fasta - >> "$scratch"/"$acc"/"$q"_"$acc"_cds_1.fa
     done < "$ref"/cds_intervals/"$q".intervals
-    echo ">${acc}" > "$scratch"/"$acc"/"$q"_"$acc".fa
-    awk '{printf $0;}' "$scratch"/"$acc"/"$q"_"$acc"_cds.fa | fold -w 60 - >> "$scratch"/"$acc"/"$q"_"$acc".fa
-    rm "$scratch"/"$acc"/"$q"_"$acc"_cds.fa
+    echo ">${acc}_0" > "$scratch"/"$acc"/"$q"_"$acc"_cds.fa
+    echo $(cat "$scratch"/"$acc"/"$q"_"$acc"_cds_0.fa) | fold -w 60 - >> "$scratch"/"$acc"/"$q"_"$acc"_cds.fa
+#    awk '{printf $0;}' "$scratch"/"$acc"/"$q"_"$acc"_cds_0.fa | fold -w 60 - >> "$scratch"/"$acc"/"$q"_"$acc".fa
+    echo ">${acc}_1" >> "$scratch"/"$acc"/"$q"_"$acc"_cds.fa
+    echo $(cat "$scratch"/"$acc"/"$q"_"$acc"_cds_1.fa) | fold -w 60 - >> "$scratch"/"$acc"/"$q"_"$acc"_cds.fa
+#    awk '{printf $0;}' "$scratch"/"$acc"/"$q"_"$acc"_cds_1.fa | fold -w 60 - >> "$scratch"/"$acc"/"$q"_"$acc".fa
+#    rm "$scratch"/"$acc"/"$q"_"$acc"_cds.fa
   done < "$ref"/"$gene_list"
 
   # move sample file directory from scratch
@@ -102,4 +110,3 @@ for i in {0..2}; do
   echo "end CDS FASTA script"
   date
 done
-
