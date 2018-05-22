f0d1ec272d204166bf20250c0431d54bbd794025
Dianne Velasco <dmvelasco@ucdavis.edu>
2018-05-22 16:26:31 -0700

    more updates to phase-2-fasta script

--- phase_bam.sh
+++ phase_bam.sh
@@ -90,14 +90,14 @@ for i in {0..1}; do
   date
 
   ##### Extract FASTA from each BAM with SAMtools #####
+  # phase 0
   echo -e "pile up sequences and call VCF for first phased BAM file, phased_0"
   date
-  # phase 0
   "$bin"/samtools mpileup -uABRx -f "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam | "$bin"/bcftools call -c -O z -o "$acc"_phased.0.vcf
   "$bin"/bcftools index -f "$acc"_phased.0.vcf
+  # phase 1
   echo -e "pile up sequences and call VCF for second phased BAM file, phased_1"
   date
-  # phase 1
   "$bin"/samtools mpileup -uABRx -f "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam | "$bin"/bcftools call -c -O z -o "$acc"_phased.1.vcf
   "$bin"/bcftools index -f "$acc"_phased.1.vcf
 
@@ -116,34 +116,25 @@ for i in {0..1}; do
     "$bin"/samtools faidx "$ref"/Prunus_persica_v1.0_scaffolds.fa "$gene_interval" | "$bin"/bcftools consensus "$acc"_phased.0.vcf -o "$scratch"/"$acc"/"$gene_id"_"$acc"_0.fa
     # extract consensus gene sequence from phase 1
     "$bin"/samtools faidx "$ref"/Prunus_persica_v1.0_scaffolds.fa "$gene_interval" | "$bin"/bcftools consensus "$acc"_phased.1.vcf -o "$scratch"/"$acc"/"$gene_id"_"$acc"_1.fa
-
-##    "$bin"/samtools mpileup -u -r "$gene_interval" -A -B -R -f "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam | "$bin"/bcftools consensus -f "$ref"/Prunus_persica_v1.0_scaffolds.fa -o "$scratch"/"$acc"/"$gene_id"_"$acc"_0.fa
-##    "$bin"/samtools mpileup -u -r "$gene_interval" -A -B -R -f "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.1.bam | "$bin"/bcftools consensus -f "$ref"/Prunus_persica_v1.0_scaffolds.fa -o "$scratch"/"$acc"/"$gene_id"_"$acc"_1.fa
-##    "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam "$gene_interval" | "$bin"/samtools fasta - > "$scratch"/"$acc"/"$gene_id"_"$acc"_0.fa
-##    "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.1.bam "$gene_interval" | "$bin"/samtools fasta - > "$scratch"/"$acc"/"$gene_id"_"$acc"_1.fa
   done < "$ref"/"$gene_pos_list"
 
   ##### process each CDS from list of gene IDs and raw GATK alternate reference FASTA maker
 
   while read q; do
-    # create phased CDS FASTA components from BAM
-    # concatenate and create final CDS FASTA with basic file manipulations
     touch "$scratch"/"$acc"/"$q"_"$acc"_cds_0_temp.fa
     touch "$scratch"/"$acc"/"$q"_"$acc"_cds_1_temp.fa
     x=1
+    # create phased CDS FASTA components from BAM
     while read r; do
       "$bin"/samtools faidx "$ref"/Prunus_persica_v1.0_scaffolds.fa "$r" | "$bin"/bcftools consensus "$acc"_phased.0.vcf -o "$scratch"/"$acc"/"$q"_"$acc"_temp"$x"_0.fa
       "$bin"/samtools faidx "$ref"/Prunus_persica_v1.0_scaffolds.fa "$r" | "$bin"/bcftools consensus "$acc"_phased.1.vcf -o "$scratch"/"$acc"/"$q"_"$acc"_temp"$x"_1.fa
-##      "$bin"/samtools mpileup -v -R "$ref"/cds_intervals/"$q".intervals -A -B -R -f "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam | "$bin"/bcftools consensus -f "$ref"/Prunus_persica_v1.0_scaffolds.fa - -o "$scratch"/"$acc"/"$q"_"$acc"_cds_0.fa
-##      "$bin"/samtools mpileup -v -R "$ref"/cds_intervals/"$q".intervals -A -B -R -f "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.1.bam | "$bin"/bcftools consensus -f "$ref"/Prunus_persica_v1.0_scaffolds.fa - -o "$scratch"/"$acc"/"$q"_"$acc"_cds_1.fa
-##      "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.0.bam "$r" | "$bin"/samtools fasta - >> "$scratch"/"$acc"/"$q"_"$acc"_cds_0.fa
-##      "$bin"/samtools view -u -T "$ref"/Prunus_persica_v1.0_scaffolds.fa "$acc"_phased.1.bam "$r" | "$bin"/samtools fasta - >> "$scratch"/"$acc"/"$q"_"$acc"_cds_1.fa
       ((x++))
       tail -n +2 "$scratch"/"$acc"/"$q"_"$acc"_temp"$x"_0.fa >> "$scratch"/"$acc"/"$q"_"$acc"_cds_0_temp.fa
       tail -n +2 "$scratch"/"$acc"/"$q"_"$acc"_temp"$x"_1.fa >> "$scratch"/"$acc"/"$q"_"$acc"_cds_1_temp.fa
       rm "$scratch"/"$acc"/"$q"_"$acc"_temp"$x"_0.fa "$scratch"/"$acc"/"$q"_"$acc"_temp"$x"_1.fa
     done < "$ref"/cds_intervals/"$q".intervals
 
+    # concatenate and create final CDS FASTA with basic file manipulations
     echo ">${acc}_0" >> "$scratch"/"$acc"/"$q"_"$acc"_cds.fa
     echo $(cat "$scratch"/"$acc"/"$q"_"$acc"_cds_0_temp.fa) | fold -w 60 - >> "$scratch"/"$acc"/"$q"_"$acc"_cds.fa
 ##  awk '{printf $0;}' "$scratch"/"$acc"/"$q"_"$acc"_cds_0.fa | fold -w 60 - >> "$scratch"/"$acc"/"$q"_"$acc".fa
