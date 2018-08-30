878f59e2454fe1e1b6d5650c49c78252bdd571db
Dianne Velasco <dmvelasco@ucdavis.edu>
2018-08-30 10:26:04 -0700

    tcoffee scripts mostly running okay, some problems with poor alignment preventing back translation

--- tcoffee_psicoffee.sh
+++ tcoffee_psicoffee.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH -D /group/jrigrp3/Velasco/Prunus/fasta/fasta-aln
-#SBATCH -o /home/dmvelasc/Projects/Prunus/slurm-log/%A_%a-stdout-tcoffee_run.txt
-#SBATCH -e /home/dmvelasc/Projects/Prunus/slurm-log/%A_%a-stderr-tcoffee_run.txt
-#SBATCH -J tcoffee
-#SBATCH -a 41-500%10
+#SBATCH -o /home/dmvelasc/Projects/Prunus/slurm-log/%A_%a-stdout-tcoffee_psi.txt
+#SBATCH -e /home/dmvelasc/Projects/Prunus/slurm-log/%A_%a-stderr-tcoffee_psi.txt
+#SBATCH -J expresso
+#SBATCH -a 2,5,6,14,23,28,29,31,34,35%2
 #SBATCH -p bigmemh
 #SBATCH -n 1
 #SBATCH -c 4
 #SBATCH --mem=32G
 #SBATCH -t 2-00:00:00
-#SBATCH --exclude=bigmem1
+#SBATCH --exclude=bigmem1,bigmem2
 #SBATCH --mail-user=dmvelasco@ucdavis.edu
 #SBATCH --mail-type=ALL
 set -e
@@ -17,6 +17,10 @@ set -u
 
 # number of total array jobs
 # -a 1-27864%50
+
+# using expresso when DNA/protein sequences differ,
+# which has occurred with
+# 2,5,6,14,23,28,31,34,35,39
 
 # Load zlib 1.2.8
 module load zlib
@@ -55,13 +59,12 @@ t_coffee -other_pg seq_reformat -in "$dna_dir"/"${gene[0]}"_cds.fa -action +tran
 
 #### STEP 3: ALIGN MULTI-SEQUENCE PEPTIDE FASTA
 # psi-coffee takes a long time, would be good if time not as much of an issue
-#t_coffee "$pep_dir"/"${gene[0]}"_cds_pep.fasta -mode psicoffee -multi_core=4 -email=dmvelasco@ucdavis.edu
+t_coffee "$pep_dir"/"${gene[0]}"_cds_pep.fasta -mode psicoffee -multi_core=4 -email=dmvelasco@ucdavis.edu
 # m-coffee equivalent still takes time but not nearly as long
 #t_coffee "$pep_dir"/"${gene[0]}"_cds_pep.fasta -method muscle_msa, probcons_msa, dialigntx_msa, clustalo_msa, pcma_msa, mafft_msa, t_coffee_msa -multi_core=8 -output fasta_aln
-t_coffee "$pep_dir"/"${gene[0]}"_cds_pep.fasta -method muscle_msa, probcons_msa, dialigntx_msa, clustalo_msa, pcma_msa, mafft_msa, t_coffee_msa -multi_core=4 -output fasta_aln
 
 #### STEP 4: BACK TRANSLATE MULTI-SEQUENCE PEPTIDE FASTA WITH ORIGINAL MULTI-SEQUENCE DNA FASTA
-t_coffee -other_pg seq_reformat -in "$dna_dir"/"${gene[0]}"_cds.fa -in2 "$pep_aln_dir"/"${gene[0]}"_cds_pep.fasta_aln -action +thread_dna_on_prot_aln -output fasta_aln > "$dna_aln_dir"/"${gene[0]}"_cds.aln
+t_coffee -other_pg seq_reformat -in "$dna_dir"/"${gene[0]}"_cds.fa -in2 "$pep_aln_dir"/"${gene[0]}"_cds_pep.aln -action +thread_dna_on_prot_aln -output fasta_aln > "$dna_aln_dir"/"${gene[0]}"_cds.aln
 
 echo -e "end T-Coffee cycle for CDS sequence ${gene[0]}"
 date
